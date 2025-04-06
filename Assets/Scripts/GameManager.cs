f6ef00c939c74e964b6eba6eab93d967ba83c765
Patrick Hähnel <p_haehnel@hotmail.de>
2025-04-06 21:18:54 +0200

    edit: add first variables to GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using TMPro;
 
 namespace KarnesDevelopment
 {
     public class GameManager : MonoBehaviour
     {
+        [Header("Money Variables")]
+        public TextMeshProUGUI moneyLabel;
+        public int moneyValue;
+        [Header("Game Variables")]
+        public TextMeshProUGUI gameLabel;
+        [Header("Bet Variables")]
+        public TextMeshProUGUI betValue;
+
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
+            moneyLabel.text = $"${moneyValue}";
 
         }
 
